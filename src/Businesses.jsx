5cc3a8ac09fa9e3933625c1f8775a054417b78a0
fibini <fabienswift@gmail.com>
2023-05-11 19:24:56 -0400

    adjust css

--- src/Businesses.jsx
+++ src/Businesses.jsx
@@ -8,16 +8,17 @@ export default function Businesses()
 {
     return(
         <>
-        <section id="businesses" className="h-screen">
+        <section id="businesses" className="mt-10">
           <div id="busi" className="bg-black text-white">
           <h2 className="text-3xl font-bold text-center py-5">
               My Businesses
           </h2>
           </div>
           <div className="h-96 2lg:h-96">
-          <Carousel slideInterval={10000}>
+          <Carousel slideInterval={5000}>
             <a href="https://www.facebook.com/affordableadvertising20" target="_blank" rel="noreferrer">
               <img
+              className="h-96"
               src={affordableAdvertising}
               alt="advertising"
               />
