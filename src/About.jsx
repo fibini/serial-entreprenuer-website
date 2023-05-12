8913327aae56f303081b3dc0841dd0e0de99ba6f
fibini <fabienswift@gmail.com>
2023-05-12 14:04:54 -0400

    add all businesses to carousel

--- src/About.jsx
+++ src/About.jsx
@@ -13,7 +13,7 @@ export default function About()
                     <img className="border-2 rounded-md h-80 w-60" src={portrait} alt="portrait"/> 
                 </div>
                 <div className="bg-black text-white text-lg border-2 rounded-md">
-                    <p>Hi my name is Tanukeuno-Ashock Rowe, I am also known as Tanuke(Tan-new-key). I am a serial entrepreneur, this means I am the CEO of many businesses. These businesses include Real Estate(sales and rentals), Property Management, Concierge services, Taxi and Hired car services, Recruiting services, Self Improvement, Weight loss, Hormonal balence, Dog(sales), Security services and even Cosmetics and Freighting.</p>
+                    <p>Hi my name is Tanukeuno-Ashock Rowe, I am also known as Tanuke(Tan-new-kay). I am a serial entrepreneur, this means I am the CEO of many businesses. These businesses include Real Estate(sales and rentals), Property Management, Concierge services, Taxi and Hired car services, Recruiting services, Self Improvement, Weight loss, Hormonal balence, Dog(sales), Security services and even Cosmetics and Freighting.</p>
                 </div>
             </div>
         </section>
--- src/Businesses.jsx
+++ src/Businesses.jsx
@@ -3,6 +3,10 @@ import apply from './assets/apply-yourself.png'
 import affordableAdvertising from './assets/advertising.png'
 import tanup from './assets/tanup.png'
 import slimchances from './assets/slimchances.png'
+import trwSecurity from './assets/security.jpg'
+import finao from './assets/finao.png'
+import wiggin from './assets/wiggin.png'
+import furrever from './assets/furrever.png'
 
 export default function Businesses()
 {
@@ -18,14 +22,14 @@ export default function Businesses()
           <Carousel slideInterval={5000}>
             <a href="https://www.facebook.com/affordableadvertising20" target="_blank" rel="noreferrer">
               <img
-              className="h-96"
+              className="h-96 w-screen"
               src={affordableAdvertising}
               alt="advertising"
               />
             </a>
             <a href="https://www.facebook.com/Applyyourselfnow876" target="_blank" rel="noreferrer">
               <img
-              className="h-96"
+              className="h-96 w-screen"
               src={apply}
               alt="applyimg"
               />
@@ -39,15 +43,39 @@ export default function Businesses()
             </a>
             <a href="https://www.facebook.com/profile.php?id=100064482830532" target="_blank" rel="noreferrer">
               <img
-                className="h-96"
+                className="h-96 w-screen"
                 src={slimchances}
                 alt="weight-loss"
               />
             </a>
-            <img
-              src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-              alt="..."
-            />
+            <a href="https://www.facebook.com/profile.php?id=100070131725399" target="_blank" rel="noreferrer">
+              <img
+                className="h-96 w-screen"
+                src={trwSecurity}
+                alt="security-firm"
+              />
+            </a>
+            <a href="https://www.facebook.com/FINAOCONQUERREALTY" target="_blank" rel="noreferrer">
+              <img
+                className="h-96 w-screen"
+                src={finao}
+                alt="real-estate"
+              />
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=100063960162770" target="_blank" rel="noreferrer">
+              <img
+                className="h-96 w-screen"
+                src={wiggin}
+                alt="wigs-cosmetics"
+              />
+            </a>
+            <a href="https://www.instagram.com/furreverpuppiesrhewardfhengshu/" target="_blank" rel="noreferrer">
+              <img
+                className="h-96 w-screen"
+                src={furrever}
+                alt="puppies"
+              />
+            </a>
           </Carousel>
         </div>
       </section>
