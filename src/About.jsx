7e8240c5d057350ca6de88f10fe2f32eed2fc65a
fibini <fabienswift@gmail.com>
2023-05-13 10:56:19 -0400

    adjust css

--- src/About.jsx
+++ src/About.jsx
@@ -12,7 +12,7 @@ export default function About()
                 <div className="flex justify-center mb-5">
                     <img className="border-2 rounded-md sm:h-80 w-60 md:max-w-screen-md" src={portrait} alt="portrait"/> 
                 </div>
-                <div className="bg-black text-white text-xl border-2 rounded-md h-80 lg:w-1/2">
+                <div className="bg-black text-white border-2 rounded-md h-80 lg:w-1/2 text-lg">
                     <p>Hi my name is Tanukeuno-Ashock Rowe, I am also known as Tanuke(Tan-new-kay). I am a serial entrepreneur, this means I am the CEO of many businesses. These businesses include Real Estate(sales and rentals), Property Management, Concierge services, Taxi and Hired car services, Recruiting services, Self Improvement, Weight loss, Hormonal balence, Dog(sales), Security services and even Cosmetics and Freighting.</p>
                 </div>
             </div>
